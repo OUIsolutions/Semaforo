e232e460f7f056bbc0b296cf4525d86e4331da82
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-02-09 12:13:19 -0300

    start struct

---
+++ globals.hpp
@@ -0,0 +1,8 @@
+//
+// Created by mateusmoutinho on 09/02/24.
+//
+
+#ifndef SEMAFORO_GLOBAIS_H
+#define SEMAFORO_GLOBAIS_H
+
+#endif //SEMAFORO_GLOBAIS_H
---
+++ main.cpp
@@ -0,0 +1,4 @@
+
+#include "imports.hpp"
+#include "declaration.hpp"
+#include "definition.hpp"
