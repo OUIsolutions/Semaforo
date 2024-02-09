c8bfabe9e9260cc99c49eb885e56ea83708c8cf9
Samuel Santos Gomes de Souza <Samuels.g.desouza@gmail.com>
2024-02-09 12:50:39 -0300

    att

--- main.cpp
+++ main.cpp
@@ -8,9 +8,16 @@ using namespace std;
 int main(int argc, char *argv[]){
     cli::Parser parser(argc, argv);
 
-    parser.set_optional<int>("n", "number", 8, "Integers in all forms, e.g., unsigned int, long long, ..., are possible. Hexadecimal and Ocatl numbers parsed as well");
+    parser.set_optional<int>("t", "timeout", 60, "max time that current process can be with the ownership");
+    parser.set_optional<int>("w", "wait", 60, "max time for waiting the ownershil (after returns an error)");
+    parser.set_optional<string>("s", "storage", ".lock.json", "the entity locks json to control what its been locked");
+    parser.run_and_exit_if_error();
 
     //auto will be int
-    auto number = parser.get<int>("n");
-    cout << "Command: " << number << "\n";
+    int timeout = parser.get<int>("t");
+    int wait = parser.get<int>("w");
+    string storage = parser.get<string>("s");
+    cout << "Command: " << timeout << "\n";
+    cout << "Command: " << wait << "\n";
+    cout << "Command: " << storage << "\n";
 }
