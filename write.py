e2766a437cdb6925f874b971bdcab5c91f43150e
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-02-10 02:14:16 -0300

    att

---
+++ write.py
@@ -0,0 +1,21 @@
+from os import system
+from multiprocessing import Process, Queue
+
+def my_function(x):
+    system('./a.out --action lock --entity a.txt')
+    with open('teste.txt','r') as arq:
+        result = arq.read()
+
+
+    with open('teste.txt','w') as arq:
+        arq.write(result + x + "\n")
+    system('./a.out --action unlock --entity a.txt')
+
+
+
+with open('teste.txt','w') as arq:
+    arq.write('')
+    
+for x in range(0,10):
+    p = Process(target=my_function, args=(f'{x}'))
+    p.start()
