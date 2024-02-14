e2a61c3ac7d3d93254f2e19b083105ab6cc5dcc7
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-02-14 02:15:26 -0300

    att

--- write.py
+++ write.py
@@ -1,62 +1,53 @@
 from os import system
-from multiprocessing import Process, Queue
+from multiprocessing import Process
 from os import system
 import requests
 import json
 from os import makedirs
 from os.path import isfile
+from os.path import isdir
+from os import listdir
+def generate_list():
 
-def cap_url(worker_id):
-    while True:
-        system('./a.out --action lock --entity new_teste.json')
+    while True:    
+
         with open('new_teste.json','r') as arq:
-            result = json.loads(arq.read())
-
-        for url in result:
-            
-            retorno = system(f'./a.out -w 0 --action lock --entity {url}')
-            
-            if retorno == 0:
-                system('./a.out --action unlock --entity new_teste.json')
-                filename = f'result/{url.replace(" ", "").replace("/", "")}.json';
-
-                #means it already get
-                if(isfile(filename)):
-                    system('./a.out --action lock --entity new_teste.json')
-                    continue
-                
-                response = requests.get(url)
-                print(f"worker: {worker_id} Url: {url} Status: {response.status_code}")
-
-                with open(filename,'w') as arq:
-                    arq.write(response.text)
-
-                system('./a.out --action lock --entity new_teste.json')
-
-                #removendo a url
-                with open('new_teste.json','r') as arq:
-                    result:list = json.load(arq)
-
-                
-                    result.remove(url)
-
-                with open('new_teste.json','w') as arq:
-                    json.dump(result, arq)
-
-
-
-                system(f'./a.out -w 0 --action unlock --entity {url}')
-     
+            itens = json.loads(arq.read())
+        for e in itens:
+            filename = f'listage/{e}.txt'
+            system(f'./a.out -w 0 --action lock --entity {filename}')
+            with open(filename,'w') as arq:
+                arq.write(e)
+            system(f'./a.out --action unlock --entity {filename}')
 
         
-        system('./a.out --action unlock --entity new_teste.json')
+def cap_url(worker_id):
+    while True:
+        itens = listdir('listage')
+        for e in itens:
+            current_path = f'listage/{e}'
+            system(f'./a.out -w 0 --action lock --entity {current_path}')
+            with open(current_path,'r') as arq:
+                url = arq.read()
+            content = requests.get(url)
+            formated_url = url.replace('/','').replace(' ','')
+            with open(f'result/{formated_url}','w') as arq:
+                arq.write(content.text)
+            system(f'./a.out -w 0 --action unlock --entity {current_path}')
+
         
 
 if not isfile('new_teste.json'):
     with open('new_teste.json','w') as arq:
         arq.write('[]')
 
+makedirs('listage',exist_ok=True)
+
 makedirs('result',exist_ok=True)
-for x in range(0,2):
-    p = Process(target=cap_url, args=(str(x),))
+
+initial = Process(target=generate_list)
+initial.start()
+for i in range(0,2):
+    p = Process(target= cap_url)
     p.start()
+    
